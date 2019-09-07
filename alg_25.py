5d0e4b69416aa8059f6636bf07ab62605386529b
donrivers <271909175@qq.com>
2019-09-07 09:30:24 +0800

    Create alg_25.py

---
+++ alg_25.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
+        cur = head
+        count = 0
+        while cur and count!=k:
+            cur = cur.next
+            count += 1
+        if count == k:
+            cur = self.reverseKGroup(cur, k)
+            while count:
+                tmp = head.next
+                head.next = cur
+                cur = head
+                head = tmp
+                count -= 1
+            head = cur
+        return head
